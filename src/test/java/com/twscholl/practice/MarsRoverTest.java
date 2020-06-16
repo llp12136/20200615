50a09df7908b91421f8a0195eb253b210b0565e8
llp12136 <liliping@zybank.com.cn>
2020-06-16 11:13:49 +0800

    marsrover_M4

--- src/test/java/com/twscholl/practice/MarsRoverTest.java
+++ src/test/java/com/twscholl/practice/MarsRoverTest.java
@@ -27,20 +27,83 @@ public class MarsRoverTest {
         assertThat(coordinate.getDirection(), is("N"));
     }
     @Test
-    public void should_return_vocation_00N_when_order_L_given_00W(){
-        //Give
-
+    public void should_return_vocation_00W_when_order_M_given_10W(){
+        //Given
+        String direction = "W";
+        int X = 0;
+        int Y = 0;
+        MarsRoverCoordinate currentCoordinate = new MarsRoverCoordinate();
+        currentCoordinate.setLocalX(X);
+        currentCoordinate.setLocalY(Y);
+        currentCoordinate.setDirection(direction);
+        MarsRover marsRover = new MarsRover();
 
         //When
+        MarsRoverCoordinate coordinate = marsRover.move(currentCoordinate);
 
         //Then
+        assertThat(coordinate.getLocalX(), is(-1));
+        assertThat(coordinate.getLocalY(), is(0));
+        assertThat(coordinate.getDirection(), is("W"));
     }
     @Test
-    public void should_return_vocation_00N_when_order_R_given_00S(){
-        //Give
+    public void should_return_vocation_00E_when_order_M_given__10E(){
+        //Given
+        String direction = "E";
+        int X = 0;
+        int Y = 0;
+        MarsRoverCoordinate currentCoordinate = new MarsRoverCoordinate();
+        currentCoordinate.setLocalX(X);
+        currentCoordinate.setLocalY(Y);
+        currentCoordinate.setDirection(direction);
+        MarsRover marsRover = new MarsRover();
 
         //When
+        MarsRoverCoordinate coordinate = marsRover.move(currentCoordinate);
 
         //Then
+        assertThat(coordinate.getLocalX(), is(1));
+        assertThat(coordinate.getLocalY(), is(0));
+        assertThat(coordinate.getDirection(), is("E"));
+    }
+    @Test
+    public void should_return_vocation_00S_when_order_M_given_0_1S(){
+        //Given
+        String direction = "S";
+        int X = 0;
+        int Y = 0;
+        MarsRoverCoordinate currentCoordinate = new MarsRoverCoordinate();
+        currentCoordinate.setLocalX(X);
+        currentCoordinate.setLocalY(Y);
+        currentCoordinate.setDirection(direction);
+        MarsRover marsRover = new MarsRover();
+
+        //When
+        MarsRoverCoordinate coordinate = marsRover.move(currentCoordinate);
+
+        //Then
+        assertThat(coordinate.getLocalX(), is(0));
+        assertThat(coordinate.getLocalY(), is(-1));
+        assertThat(coordinate.getDirection(), is("S"));
+    }
+    @Test
+    public void should_return_vocation_00N_when_order_L_given_00W(){
+        //Given
+        String direction = "N";
+        int X = 0;
+        int Y = 0;
+        MarsRoverCoordinate currentCoordinate = new MarsRoverCoordinate();
+        currentCoordinate.setLocalX(X);
+        currentCoordinate.setLocalY(Y);
+        currentCoordinate.setDirection(direction);
+        MarsRover marsRover = new MarsRover();
+
+        //When
+        MarsRoverCoordinate coordinate = marsRover.move(currentCoordinate);
+
+        //Then
+        assertThat(coordinate.getLocalX(), is(0));
+        assertThat(coordinate.getLocalY(), is(0));
+        assertThat(coordinate.getDirection(), is("W"));
     }
 }
